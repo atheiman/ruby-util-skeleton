dfb102e073f56680a4dcf5aeac80e3fecfcc248d
Austin Heiman <atheimanksu@gmail.com>
2015-12-03 14:48:32 -0600

    updated logger to instance variable

--- util.rb
+++ util.rb
@@ -31,23 +31,23 @@ load options[:defaults]
 @config = @defaults.merge!(@config)
 
 # create logger
-logger = Logger.new(@config[:log_location])
-logger.level = Logger.const_get(@config[:log_level])
-logger.formatter = proc do |severity, datetime, progname, msg|
+@logger = Logger.new(@config[:log_location])
+@logger.level = Logger.const_get(@config[:log_level])
+@logger.formatter = proc do |severity, datetime, progname, msg|
   "[#{datetime.strftime('%Y-%m-%d %H:%M:%S')}] #{severity}: #{msg}\n"
 end
 
 # load items
 json = File.read(@config[:items_filename])
 items = JSON.parse(json, :symbolize_names => true)
-logger.debug { "Loaded items from json: #{items}" }
+@logger.debug { "Loaded items from json: #{items}" }
 
 # do something with the items you loaded
 @responses = []
 items.each do |item|
-  logger.info { "processing item #{item}" }
+  @logger.info { "processing item #{item}" }
   url = @config[:api_url_base] + item[:id]
-  logger.debug { "sending GET to #{url}" }
+  @logger.debug { "sending GET to #{url}" }
   resp = Net::HTTP.get_response(URI.parse(url))
   @responses << JSON.parse(resp.body)
 end
